1e353ee8f746a555f5ac5f5a3f3fa99e6fc0cb04
slavonicblin <78567665+slavonicblin@users.noreply.github.com>
2021-02-23 19:12:09 -0600

    Update script.js
    stoopid mistake

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // this is the scripts file
 console.log("hello vadim");
 document.getElementById("btn1").onclick = function() {
-	console.log("button is working comrade"
+	console.log("button is working comrade")
 }
