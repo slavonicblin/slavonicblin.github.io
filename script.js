21fd0c69ab89c6257f46afd3506e231c162e472b
slavonicblin <78567665+slavonicblin@users.noreply.github.com>
2021-02-23 19:10:47 -0600

    edits
    move script. create button functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // this is the scripts file
 console.log("hello vadim");
-document.getElementById("btn1")
+document.getElementById("btn1").onclick = function() {
+	console.log("button is working comrade"
+}
